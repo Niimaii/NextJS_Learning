77669e2ab30b3b71ab8465ac9a05ea08d0258e6e
Niimaii <nimaicgomes@gmail.com>
2023-09-06 23:30:24 -0300

    fixed BlogPost type

--- next06/app/components/Navbar.tsx
+++ next06/app/components/Navbar.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { FaYoutube, FaTwitter, FaGithub, FaLaptop } from 'react-icons/fa';
+
 export default function Navbar() {
   return (
     <nav className='bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10'>
@@ -8,9 +10,35 @@ export default function Navbar() {
             href='/'
             className='text-white/90 no-underline hover:text-white'
           >
-            Nimai
+            Nemo
           </Link>
         </h1>
+        <div className='flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl'>
+          <Link
+            className='text-white/90 hover:text-white'
+            href='https://www.youtube.com/@DaveGrayTeachesCode'
+          >
+            <FaYoutube />
+          </Link>
+          <Link
+            className='text-white/90 hover:text-white'
+            href='https://courses.davegray.codes/'
+          >
+            <FaLaptop />
+          </Link>
+          <Link
+            className='text-white/90 hover:text-white'
+            href='https://github.com/gitdagray'
+          >
+            <FaGithub />
+          </Link>
+          <Link
+            className='text-white/90 hover:text-white'
+            href='https://twitter.com/yesdavidgray'
+          >
+            <FaTwitter />
+          </Link>
+        </div>
       </div>
     </nav>
   );
