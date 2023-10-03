a8889a358a2e6ffef006cca69f1b725a3bd3d652
Niimaii <nimaicgomes@gmail.com>
2023-10-03 18:52:06 -0300

    next09 finished

--- next09/src/app/api/todos/[id]/route.ts
+++ next09/src/app/api/todos/[id]/route.ts
@@ -1,10 +1,16 @@
 import { NextResponse } from 'next/server';
 
 const DATA_SOURCE_URL = 'https://jsonplaceholder.typicode.com/todos';
+type Props = {
+  params: {
+    id: string;
+  };
+};
 
-export async function GET(request: Request) {
+export async function GET(request: Request, { params: { id } }: Props) {
   // Get everything after the last '/' in the url
-  const id = request.url.slice(request.url.lastIndexOf('/') + 1);
+  // const id = request.url.slice(request.url.lastIndexOf('/') + 1);
+
   const res = await fetch(`${DATA_SOURCE_URL}/${id}`);
   const todo: Todo = await res.json();
 
